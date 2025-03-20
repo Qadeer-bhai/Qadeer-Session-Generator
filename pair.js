8b5e1821c008276b39dfa7dcc4e883ff7df7df2f
Qadeer-bhai <ummerkulachi@gmail.com>
2025-03-21 00:57:26 +0500

    Add files via upload

---
+++ pair.js
@@ -0,0 +1,113 @@
+const PastebinAPI = require('pastebin-js'),
+pastebin = new PastebinAPI('EMWTMkQAVfJa9kM-MRUrxd5Oku1U7pgL')
+const {makeid} = require('./id');
+const express = require('express');
+const fs = require('fs');
+let router = express.Router()
+const pino = require("pino");
+const {
+    default: Gifted_Tech,
+    useMultiFileAuthState,
+    delay,
+    makeCacheableSignalKeyStore,
+    Browsers
+} = require("maher-zubair-baileys");
+
+function removeFile(FilePath){
+    if(!fs.existsSync(FilePath)) return false;
+    fs.rmSync(FilePath, { recursive: true, force: true })
+ };
+router.get('/', async (req, res) => {
+    const id = makeid();
+    let num = req.query.number;
+        async function GIFTED_MD_PAIR_CODE() {
+        const {
+            state,
+            saveCreds
+        } = await useMultiFileAuthState('./temp/'+id)
+     try {
+            let Pair_Code_By_Gifted_Tech = Gifted_Tech({
+                auth: {
+                    creds: state.creds,
+                    keys: makeCacheableSignalKeyStore(state.keys, pino({level: "fatal"}).child({level: "fatal"})),
+                },
+                printQRInTerminal: false,
+                logger: pino({level: "fatal"}).child({level: "fatal"}),
+                browser: ["Chrome (Linux)", "20.0.4", "VRT"]
+             });
+             if(!Pair_Code_By_Gifted_Tech.authState.creds.registered) {
+                await delay(1500);
+                        num = num.replace(/[^0-9]/g,'');
+                            const code = await Pair_Code_By_Gifted_Tech.requestPairingCode(num)
+                 if(!res.headersSent){
+                 await res.send({code});
+                     }
+                 }
+            Pair_Code_By_Gifted_Tech.ev.on('creds.update', saveCreds)
+            Pair_Code_By_Gifted_Tech.ev.on("connection.update", async (s) => {
+                const {
+                    connection,
+                    lastDisconnect
+                } = s;
+                if (connection == "open") {
+                await delay(5000);
+                let data = fs.readFileSync(__dirname + `/temp/${id}/creds.json`);
+                await delay(800);
+               let b64data = Buffer.from(data).toString('base64');
+               let session = await Pair_Code_By_Gifted_Tech.sendMessage(Pair_Code_By_Gifted_Tech.user.id, { text: '' + b64data });
+
+               let GIFTED_MD_TEXT = `
+ 💀 *_The Realm of Pain Welcomes You...* 💀
+    ═════════════════════════════
+    
+    🤖 *_Connected by the QADEER KHAN* 
+    
+    🌩️ *VIP ENRT - Designed by the QADEER 
+         
+         QADEER KHAN KULACHI* 🌩️
+
+    ╔════════════════════════
+    ║ 🔥 *『 Welcome to the REAL 』* 🔥
+    ║
+    ║ You're foolish enough to make it here.
+    ║  Congrats, I guess.  
+    ║ Let's see if you can survive *VRT*.  
+    ╚═════════════════════════
+    
+    ⚡ *『 BOT REPO - Don’t Dare to Fork Me! 』* 
+    🔗 https://github.com/Qadeer-bhai/TOFAN-MD
+
+    ═══════════════════════
+
+    📢 *『 Join the Cult – WhatsApp Channel 』* 
+    🔗 https://whatsapp.com/channel/0029Vaw6yRaBPzjZPtVtA80A
+    
+    ════════════════════════
+
+    ⚠️ *_2025 - The Reign of QADEER'S HACKING*
+    
+    *You’re weak if you don’t give my repo a ⭐*
+`
+
+ await Pair_Code_By_Gifted_Tech.sendMessage(Pair_Code_By_Gifted_Tech.user.id,{text: GIFTED_MD_TEXT},{quoted:session})
+ 
+
+        await delay(100);
+        await Pair_Code_By_Gifted_Tech.ws.close();
+        return await removeFile('./temp/'+id);
+            } else if (connection === "close" && lastDisconnect && lastDisconnect.error && lastDisconnect.error.output.statusCode != 401) {
+                    await delay(10000);
+                    GIFTED_MD_PAIR_CODE();
+                }
+            });
+        } catch (err) {
+            console.log("service restated");
+            await removeFile('./temp/'+id);
+         if(!res.headersSent){
+            await res.send({code:"Service Unavailable"});
+         }
+        }
+    }
+    return await GIFTED_MD_PAIR_CODE()
+});
+module.exports = router
